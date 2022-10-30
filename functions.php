ed22e4fc20deba853660459e12c4c636aad1cb15
stephane0713 <stephane.dupre@ynov.com>
2022-10-30 09:45:56 +0100

    added Auth

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function auth()
-{
-  if (!isset($_SESSION['auth']) || empty($_SESSION['auth'])) {
-    header('Location: /error');
-  }
-}
--- routes.php
+++ routes.php
@@ -2,5 +2,6 @@
 require_once __DIR__ . '/router.php';
 
 get('/', 'pages/accueil');
+get('/login', 'pages/inscription');
 
 // any('/404', 'pages/404.php');
