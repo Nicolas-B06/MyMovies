9dad36670147a04135dcb39769de9ed77ce5e3d6
Nicolas-B06 <nicolas.bernard@nextsourcia.com>
2022-11-11 15:27:57 +0100

    Adding style to dashboard

--- view/admin/dashboard.php
+++ view/admin/dashboard.php
@@ -13,12 +13,39 @@ $users = $userModel->all();
 
 <?php inc_header(); ?>
 <main class="my-5">
-  <?php foreach ($users as $user) : ?>
-    <div>
-      <div><?= htmlspecialchars($user->getName()) ?></div>
-      <a href="<?= "/admin/user/" . htmlspecialchars($user->getId()) ?>">Voir profil</a>
+  <div>
+    <h1 class="text-center">Dashboard</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Email</th>
+                <th scope="col">Role</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($users as $user) : ?>
+                <tr>
+                  <th scope="row"><?= $user->getId() ?></th>
+                  <td><?= $user->getEmail() ?></td>
+                  <td><?= $user->getRole() ?></td>
+                  <td>
+                    <a href="/admin/user/<?= $user->getId() ?>" class="btn btn-primary">Voir</a>
+                    <a href="/admin/user/<?= $user->getId() ?>/delete" class="btn btn-danger">Supprimer</a>
+                  </td>
+                </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  <?php endforeach ?>
+  </div>
+  
 </main>
 
 <?php inc_footer(); ?>
